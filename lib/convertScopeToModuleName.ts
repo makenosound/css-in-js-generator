26435ba833529a4cc2ca7b4025f4bbd16739ab80
Max Wheeler <max@makenosound.com>
2020-12-03 10:41:50 +1100

    Add class to the restricted keywords

--- lib/convertScopeToModuleName.ts
+++ lib/convertScopeToModuleName.ts
@@ -4,7 +4,7 @@ export function convertScopeToModuleName(scope: string) {
     return camelCase(scope)
         .replace(/^(\d)/, "_$1")
         .replace(
-            /^(break|case|catch|continue|debugger|default|delete|do|else|finally|for|function|if|in|instanceof|new|return|switch|this|throw|try|typeof|var|void|while|with)$/,
+            /^(class|break|case|catch|continue|debugger|default|delete|do|else|finally|for|function|if|in|instanceof|new|return|switch|this|throw|try|typeof|var|void|while|with)$/,
             "_$1",
         );
 }
