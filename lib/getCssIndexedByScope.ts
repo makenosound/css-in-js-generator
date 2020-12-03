1a17e6bac85d8996f9736d9fa0db74d51276b06c
Max Wheeler <max@makenosound.com>
2020-12-03 12:11:10 +1100

    Add specific replacement rules to account for Tailwind selectors

--- lib/getCssIndexedByScope.ts
+++ lib/getCssIndexedByScope.ts
@@ -4,7 +4,20 @@ import * as Stringifier from "postcss/lib/stringifier";
 import { getNodeScopes } from "./getNodeScopes";
 import { getSelectorScope } from "./getSelectorScope";
 
+function prepassTailwindSelectors(css: string): string {
+    return css
+        // .\32xl\:rule -> .2xl\:rule
+        .replace(/\.\\3/g, ".")
+        // something\:rule -> .something-rule
+        .replace(/\\:/g, "-")
+        // .rule-0\.5 -> .rule-0dot5
+        .replace(/([a-z]\-\d+)\\\.(\d+)/g, "$1dot$2")
+        // .rule-1\/3 -> .rule-1of3
+        .replace(/([a-z]\-\d+)\\\/(\d+)/g, "$1of$2");
+}
+
 export function getCssIndexedByScope(css: string): Map<string, string> {
+    css = prepassTailwindSelectors(css);
     const cssIndexedByScope = new Map();
 
     const scopesStack = [new Set(["root"])];
